ce391a1a30025568619f5e0a20da5bc99222017b
tropicalfishes <rdygzs@163.com>
2015-07-15 01:30:18 +0800

    version 0.0.1

--- net.py
+++ net.py
@@ -1,1 +1,13 @@
+#using utf_8
+import httplib
+import time
+
+g_ConnectionList = []
+
+def	RequestData(addr, port=None, cb):
+	conn = httplib.HTTPConnection(addr, port)
+	print "请求开始时间", time.time()
+	conn.request("GET", '/')
+	print "请求结束时间"
+
 
